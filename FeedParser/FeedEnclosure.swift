3c2560d673a2049b45776d6540a15c92deaabe5f
yavuz <yvzyldrm@gmail.com>
2015-01-04 16:09:24 +0200

    enclosures attr length to integer

--- FeedParser/FeedEnclosure.swift
+++ FeedParser/FeedEnclosure.swift
@@ -10,9 +10,9 @@ import UIKit
 class FeedEnclosure: NSObject {
     var url: String
     var type: String
-    var length: String
+    var length: Int
     
-    init(url: String, type: String, length: String) {
+    init(url: String, type: String, length: Int) {
         self.url = url
         self.type = type
         self.length = length
